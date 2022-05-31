fdf576a0d7303d7daea2a94806d6103d4eeea3d5
wangyan66 <819156618@qq.com>
2022-05-30 23:48:30 -0700

    update website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,81 @@
-(function(a){var c=a(window),b=a("body");a(window).on("scroll",function(){var i=a(window).scrollTop();if(i<100){a(".sticky-header").removeClass("sticky")}else{a(".sticky-header").addClass("sticky")}});(function(){var l=a(".offcanvas-toggle"),j=a(".offcanvas"),k=a(".offcanvas-overlay"),i=a(".mobile-menu-toggle");l.on("click",function(o){o.preventDefault();var n=a(this),m=n.attr("href");b.addClass("offcanvas-open");a(m).addClass("offcanvas-open");k.fadeIn();if(n.parent().hasClass("mobile-menu-toggle")){n.addClass("close")}});a(".offcanvas-close, .offcanvas-overlay").on("click",function(m){m.preventDefault();b.removeClass("offcanvas-open");j.removeClass("offcanvas-open");k.fadeOut();i.find("a").removeClass("close")})})();function e(){var i=a(".offcanvas-menu"),j=i.find(".mobile-sub-menu");j.parent().prepend('<div class="offcanvas-menu-expand"></div>');i.on("click","li a, .offcanvas-menu-expand",function(l){var k=a(this);if(k.attr("href")==="#"||k.hasClass("offcanvas-menu-expand")){l.preventDefault();if(k.siblings("ul:visible").length){k.parent("li").removeClass("active");k.siblings("ul").slideUp();k.parent("li").find("li").removeClass("active");k.parent("li").find("ul:visible").slideUp()}else{k.parent("li").addClass("active");k.closest("li").siblings("li").removeClass("active").find("li").removeClass("active");k.closest("li").siblings("li").find("ul:visible").slideUp();k.siblings("ul").slideDown()}}})}e();a("select").niceSelect();var g=new Swiper(".service-display-slider .swiper-container",{slidesPerView:3,speed:1500,loop:true,spaceBetween:45,pagination:{el:".service-display-dots .swiper-pagination",clickable:true,},breakpoints:{320:{slidesPerView:1,},768:{slidesPerView:2,},1200:{slidesPerView:3,},}});var f=new Swiper(".project-display-slider .swiper-container",{spaceBetween:50,effect:"slide",speed:1500,navigation:{nextEl:".project-display-box .button-next",prevEl:".project-display-box .button-prev",},breakpoints:{0:{slidesPerView:1,},768:{slidesPerView:2,},992:{slidesPerView:2,},1800:{centeredSlides:true,slidesPerView:3,}}});var h=new Swiper(".testimonial-display-slider .swiper-container",{slidesPerView:2,loop:true,navigation:{nextEl:".testimonial-display-box .button-next",prevEl:".testimonial-display-box .button-prev",},breakpoints:{0:{slidesPerView:1,},992:{spaceBetween:80,slidesPerView:2,},1200:{spaceBetween:100,},1400:{spaceBetween:150,},1800:{spaceBetween:175,}}});var d=new Swiper(".company-logo-display-slider .swiper-container",{slidesPerView:4,loop:true,breakpoints:{0:{slidesPerView:1,},480:{slidesPerView:2,spaceBetween:50,},576:{slidesPerView:2,spaceBetween:50,},768:{slidesPerView:3,spaceBetween:50,},992:{slidesPerView:3,spaceBetween:60,},1200:{slidesPerView:4,spaceBetween:85,}}});a(".counter").counterUp({delay:10,time:1000});a(".wave-btn").venobox();a(".projects-wrapper-gallery-content").imagesLoaded(function(){a(".projects-gallery-filter-nav").on("click","button",function(){var j=a(this).attr("data-filter");i.isotope({filter:j});a(this).siblings(".active").removeClass("active");a(this).addClass("active")});var i=a(".projects-wrapper-gallery-content").isotope({itemSelector:".filter-item",percentPosition:true,})});if(a(".progress-line").length){a(".progress-line").appear(function(){var i=a(this);var j=i.data("width");a(i).css("width",j+"%")},{accY:0})}a("body").materialScrollTop();a('a[href="#"]').on("click",(function(i){i.preventDefault?i.preventDefault():i.returnValue=false}))})(jQuery);
+// theme switch
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.textContent = selectedTheme === 'dark' ? "Light" : "Dark"
+}
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  if (document.body.classList.contains(darkTheme)) {
+    themeButton.textContent = "Light"
+  }
+  else {
+    themeButton.textContent = "Dark"
+  }
+})
+
+
+// date or time display
+var container = document.getElementById("container")
+
+function getDateOrTime (op) {
+  if (op == "Date") {
+    var d = new Date()
+    var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    var months =
+      [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+      ]
+    var dateElement = document.createElement("h3")
+    var dateText = document.createTextNode(days[d.getDay()] + " " + months[d.getMonth()] + " " + d.getDate() + " " + d.getFullYear())
+    dateElement.appendChild(dateText)
+    return dateElement
+  } else {
+    var timeElement = document.createElement("h3")
+
+    var time = new Date()
+    var timeText = document.createTextNode(time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }))
+    timeElement.appendChild(timeText)
+    return timeElement
+  }
+
+}
+
+var form = document.querySelector("form")
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault()
+  var submitBtn = document.getElementById("submit_btn")
+  var data = new FormData(form)
+  var output = ""
+  for (const entry of data) {
+    output = output + entry[1]
+  };
+
+  var displayElement = getDateOrTime(output)
+  if (container.childNodes.length != 0) {
+    container.innerHTML = ""
+    submitBtn.textContent = "Show"
+
+  } else {
+    container.appendChild(displayElement)
+    submitBtn.textContent = "Hide"
+    console.log("hi")
+  }
+
+}, false)
